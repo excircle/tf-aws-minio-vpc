4a90a289c31e9c1ed7a64b3fc0bbfaa8a5c62eaf
excircle <unixdisciple.us@gmail.com>
2024-11-04 12:25:35 -0800

    Adding VPC id output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,16 @@
 output "vpc_az_data" {
     value = data.aws_availability_zones.available
 }
+
+output "subnets" {
+    value = {
+        // Public Subnets
+        public = values({for k, v in aws_subnet.public: k => v.id})
+        // if var.make_private is true, return private subnets
+        private = var.make_private ? values({for k, v in aws_subnet.private: k => v.id}) : null
+    }
+}
+
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
